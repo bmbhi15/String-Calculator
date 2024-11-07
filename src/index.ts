fee11c7e7dd2d636e3554f7fddbb11e87d1173d0
Admin <admin@Admins-MacBook-Pro.local>
2024-11-07 21:18:44 +0530

    Refactored code and made the tests pass

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 function add(numbers: string): number {
   if (numbers === "") {
     return 0;
-  } else if (numbers === "1") {
-    return 1;
   }
-  return 0;
+  return parseInt(numbers);
 }
 
 export { add };
