52a85efcdefaf36ef56dbabca20f8cac15c61dca
Admin <admin@Admins-MacBook-Pro.local>
2024-11-07 21:59:17 +0530

    Refactored the code to support custom delimiter

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 function add(numbers: string): number {
-  numbers = numbers.replace(/\s/g, "");
-  console.log(numbers);
   if (numbers === "") {
     return 0;
   }
-  if (numbers.includes(",")) {
-    const nums = numbers.split(",").map((num) => parseInt(num));
+  let delimiter = ",";
+  if (numbers.startsWith("//")) {
+    const firstNewLine = numbers.indexOf("\n");
+    delimiter = numbers.substring(2, firstNewLine);
+    numbers = numbers.substring(firstNewLine + 1);
+  }
+  numbers = numbers.replace(/\s/g, "");
+  if (numbers === "") {
+    return 0;
+  }
+  if (numbers.includes(delimiter)) {
+    const nums = numbers.split(delimiter).map((num) => parseInt(num));
     return nums.reduce((sum, num) => sum + num, 0);
   }
   return parseInt(numbers);
