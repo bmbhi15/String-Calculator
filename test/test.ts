7a3b7904223e1834f87882ace7cff5b925e23205
Admin <admin@Admins-MacBook-Pro.local>
2024-11-07 21:17:05 +0530

    Added and failed tests for single digit numbers

--- test/test.ts
+++ test/test.ts
@@ -4,8 +4,37 @@ describe("String Calculator", function () {
   it("Should be 0 for empty string", function () {
     assert.equal(add(""), 0);
   });
-
+  it("Should be 0 for 0", function () {
+    assert.equal(add("0"), 0);
+  });
   it("Should be 1 for 1", function () {
     assert.equal(add("1"), 1);
+  });
+  it("Should be 2 for 2", function () {
+    assert.equal(add("2"), 2);
+  });
+
+  it("Should be 3 for 3", function () {
+    assert.equal(add("3"), 3);
+  });
+
+  it("Should be 4 for 4", function () {
+    assert.equal(add("4"), 4);
+  });
+
+  it("Should be 5 for 5", function () {
+    assert.equal(add("5"), 5);
+  });
+  it("Should be 6 for 6", function () {
+    assert.equal(add("6"), 6);
+  });
+  it("Should be 7 for 7", function () {
+    assert.equal(add("7"), 7);
+  });
+  it("Should be 8 for 8", function () {
+    assert.equal(add("8"), 8);
+  });
+  it("Should be 9 for 9", function () {
+    assert.equal(add("9"), 9);
   });
 });
