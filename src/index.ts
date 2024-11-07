40466321d8358296a377133b4203710601628737
Admin <admin@Admins-MacBook-Pro.local>
2024-11-07 21:31:33 +0530

    Refactored the code and made it pass

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,10 @@ function add(numbers: string): number {
   if (numbers === "") {
     return 0;
   }
+  if (numbers.includes(",")) {
+    const nums = numbers.split(",").map((num) => parseInt(num));
+    return nums.reduce((sum, num) => sum + num, 0);
+  }
   return parseInt(numbers);
 }
 
