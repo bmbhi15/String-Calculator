ce7c531399369c48f24ada7adb0981c5481c7537
Admin <admin@Admins-MacBook-Pro.local>
2024-11-07 21:11:34 +0530

    Test failed for 1

--- test/test.ts
+++ test/test.ts
@@ -4,4 +4,8 @@ describe("String Calculator", function () {
   it("Should be 0 for empty string", function () {
     assert.equal(add(""), 0);
   });
+
+  it("Should be 1 for 1", function () {
+    assert.equal(add("1"), 1);
+  });
 });
